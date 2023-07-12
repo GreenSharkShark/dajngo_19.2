57b5a942c84692bea532424b48fceb79b25b245b
Zhorik Zeniuk <ZhorikZeniuk@gmail.ru>
2023-07-12 18:11:37 +0200

    configured admin panel

--- catalog/models.py
+++ catalog/models.py
@@ -10,11 +10,15 @@ class Category(models.Model):
     описание.
     """
     category_name = models.CharField(max_length=100, verbose_name='Категория')
-    category_description = models.TextField()
+    category_description = models.TextField(verbose_name='Описание категории товара')
     created_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'{self.category_name}'
+        return f'{self.id} {self.category_name} '
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
 
 
 class Product(models.Model):
@@ -30,13 +34,13 @@ class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name='Имя')
     description = models.TextField(verbose_name='Описание')
     img = models.ImageField(upload_to='product_img/', verbose_name='Превью', **NULLABLE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    price = models.IntegerField()
-    date_of_creation = models.DateTimeField(auto_now_add=True)
-    last_change_date = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория товара')
+    price = models.IntegerField(verbose_name='Цена')
+    date_of_creation = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    last_change_date = models.DateTimeField(auto_now=True, verbose_name='Последнее изменение')
 
     def __str__(self):
-        return f'{self.name} {self.category}'
+        return f'{self.id} {self.name}, Категория: {self.category}, Цена: {self.price}'
 
     class Meta:
         verbose_name = 'продукт'
