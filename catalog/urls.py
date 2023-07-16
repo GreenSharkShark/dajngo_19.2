a601ba5f88f33894da9e8b43ab8037b54d96fa40
Zhorik Zeniuk <ZhorikZeniuk@gmail.ru>
2023-07-16 18:05:27 +0200

    homework 20.2 done

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from catalog.views import main_page, contacts_page
+from catalog.apps import CatalogConfig
+from catalog.views import main_page, contacts_page, catalog_page
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', main_page),
-    path('contacts', contacts_page)
+    path('', main_page, name='main_page'),
+    path('contacts/', contacts_page, name='contacts_page'),
+    path('catalog/', catalog_page, name='catalog_page')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
+from catalog.models import Product
 
 
 def main_page(request):
-    return render(request, 'catalog/main_page.html')
+    products_list = Product.objects.all()
+    context = {
+        'object_list': products_list,
+        'title': 'Главная'
+    }
+    return render(request, 'catalog/main_page.html', context)
+
+
+def catalog_page(request):
+    context = {
+        'title': 'Каталог'
+    }
+    return render(request, 'catalog/catalog.html', context)
 
 
 def contacts_page(request):
-    return render(request, 'catalog/contacts.html')
+    context = {
+        'title': 'Контакты'
+    }
+    return render(request, 'catalog/contacts.html', context)
 
