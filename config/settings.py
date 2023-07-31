db2b9e1716611584c35ec468af5f33eae3dc7b09
Zhorik Zeniuk <ZhorikZeniuk@gmail.ru>
2023-07-31 14:42:00 +0200

    update database pass

--- config/settings.py
+++ config/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import json
 import os
 from pathlib import Path
+
+with open('prvt.json') as file:
+    DB_PASS = json.load(file)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'products',
         'USER': 'ZhorikZeniuk',
-        'PASSWORD': '',
+        'PASSWORD': DB_PASS['pass'],
         'HOST': '',
         'PORT': 5433,
     }
