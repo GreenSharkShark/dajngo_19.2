54777dedaa652669cfb07a79d8fb76b2fa85ad60
Zhorik Zeniuk <Zhorik.me@list.ru>
2023-08-04 18:36:17 +0200

    login, logout, registration done

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
 from users.apps import UsersConfig
+from users.views import *
 
 app_name = UsersConfig.name
 
@@ -9,4 +10,6 @@ app_name = UsersConfig.name
 urlpatterns = [
     path('', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('profie/', ProfileView.as_view(), name='profile'),
 ]
