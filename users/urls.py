89e1a6cf76e650abe7b472e45d41943189d14226
Zhorik Zeniuk <Zhorik.me@list.ru>
2023-08-07 11:39:09 +0200

    реализовал права доступа к контроллерам

--- users/urls.py
+++ users/urls.py
@@ -8,7 +8,7 @@ app_name = UsersConfig.name
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('login', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('email-confirmation/', TemplateView.as_view(template_name='users/email_confirmation_sent.html'),
